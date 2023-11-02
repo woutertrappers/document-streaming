6d07b6dad9d19b26a52db74eb1418cee33b96fb0
Wouter Trappers <wouter.trappers@outlook.com>
2023-11-02 11:11:23 +0100

    update

--- client/transformer.py
+++ client/transformer.py
@@ -3,7 +3,7 @@ from numpy import add
 import pandas as pd
 
 
-df = pd.read_csv ('data.csv') 
+df = pd.read_csv ('data.csv', encoding="ISO-8859-1") 
 #print(df)
 
 # add a json column to the dataframe
